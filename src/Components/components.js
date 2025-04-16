fa68b59fe8040b2bf2f50caf2c40bb215562c2a2
Akaushik2710 <amankaushik2710ak@gmail.com>
2025-04-16 08:56:19 +0000

    Login/Signup Separated

--- src/Components/components.js
+++ src/Components/components.js
@@ -2,9 +2,9 @@ import Div from "./div";
 import Button from "./button";
 import Input from "./input";
 import Label from "./label";
-
+import Span from "./span";
 // Component Aggregator
 // This module imports and aggregates all the components used in the login page
 export default function components(){
-    return {Div, Button, Input, Label}
+    return {Div, Button, Input, Label, Span}
 }
